38662db14b00538f7bd9cd12ac575d13e77eec1d
alvinlukmanNH <galinx20gmail.com>
2018-09-23 19:55:36 +0700

    Relational

---
+++ src/if3/pkg10117095/latihan13/relational/IF310117095Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if3.pkg10117095.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA         : Alvin Lukman Nulhakim
+ * KELAS        : IF-3
+ * NIM          : 10117095
+ * Deklarasi    : Program ini berisi Relational antar variabel yang bertype int
+ */
+public class IF310117095Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int bil1= 10;
+        int bil2= 20;
+        System.out.println("a == b "  +(bil1 == bil2));
+        System.out.println("a != b " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
